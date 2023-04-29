8df198d6131a1aead6b246ce8dd2e2349a43354e
Will Riffe <w_riffe@protonmail.com>
2023-04-29 17:21:52 -0400

    🖊 Wrote functions fileWrite and init in index

--- assets/Index.js
+++ assets/Index.js
@@ -1,8 +1,9 @@
+/* Establishing inquirer and fs... and our SVG ls code (SVGenerate) */
 const inquirer = require('inquirer');
 const fs = require('fs');
+const SVGenerate = require('')
 
-
-
+// This is our array of objects used for user prompts
 const prompts = [
     {
         type: 'input',
@@ -16,7 +17,7 @@ const prompts = [
     },
     {
         type: 'list',
-        message: 'What border shape would you like for your logo?'
+        message: 'What border shape would you like for your logo?',
         choices: ['Square', 'Circle', 'Triangle'],
         name: 'shape',
     },
@@ -27,8 +28,8 @@ const prompts = [
     },
     {
         type: 'list',
-        message: 'What inner shape would you like for your logo?'
-        choices: ['Square', 'Circle', 'Triangle'],
+        message: 'What inner shape would you like for your logo?',
+        choices: ['Diamond', 'Circle', 'Flipped Triangle'],
         name: 'shape',
     },
     {
@@ -37,3 +38,42 @@ const prompts = [
         name: 'innershapecolor',
     },
 ];
+
+
+
+/* Defines function to write data; goes to file using fs module
+   Logs an error for the file if everything goes to $#!7.            */
+function fileWrite(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log(`Generated ${fileName}`),
+    );
+};
+
+
+
+/*  Defines func. "init," short for "initialize." This one prompts 
+    the user with the "prompts" parameter I made earlier, then 
+    logs them to the console.                                        */
+function init() {
+    inquirer.prompt(prompts).then(response => {
+        console.log(response);
+
+        /* Here we write the JSON string to a file                  */
+        const data = JSON.stringify(response, null, 1);
+        fileWrite('logo-params.json', data);
+
+        /*  Here we reference the script which will convert the 
+            JSON data into the SVG file, log it to the console,
+            then, finally, we actually generate the SVG             */
+        const svgCon = SVGenerate(JSON.parse(data));
+        console.log(svgCon); 
+        fileWrite(`./examples/logo.svg`, svgCon);
+    })
+
+    .catch((err) => {
+        console.error(err);
+
+    });
+}
+
+init();
